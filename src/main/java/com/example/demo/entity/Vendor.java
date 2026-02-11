11132af8f1ed898dfc09567d207d0b3e95366591
gatoAndres5 <andgalv@arizona.edu>
2026-02-10 21:23:49 -0700

    updated imports so mvn spring-boot:run works

--- src/main/java/com/example/demo/entity/Vendor.java
+++ src/main/java/com/example/demo/entity/Vendor.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Vendor {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
