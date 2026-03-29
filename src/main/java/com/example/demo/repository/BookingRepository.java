d849ea1558f00306c5fc1fde97042ea950a51f9b
elvinramirez122 <elvinramirez12@arizona.edu>
2026-03-29 14:33:45 -0700

    Added Booking controller, service, repository (#78)

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long>{
-    
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
