2ea41f29cf9ecfb15f68f41a0137cecb42f2265c
thomasshing <166445204+thomasshing@users.noreply.github.com>
2026-03-30 19:40:58 -0700

    Dev/thomasshing (#84)
    * Venue entity, repository, and business methods
    
    * small fix
    
    * venue controller

--- src/main/java/com/example/demo/entity/Venue.java
+++ src/main/java/com/example/demo/entity/Venue.java
@@ -1,4 +1,5 @@
 package com.example.demo.entity;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/example/demo/repository/VenueRepository.java
+++ src/main/java/com/example/demo/repository/VenueRepository.java
@@ -16,7 +16,7 @@ public interface VenueRepository extends JpaRepository<Venue, Long>{
     
     List<Venue> findByCapacityGreaterThanEqual(int capacity);
     
-    @Query("SELECT v FROM Venue v WHERE v.id NOT IN " +
+@Query("SELECT v FROM Venue v WHERE v.id NOT IN " +
            "(SELECT s.venue.id FROM ScheduleItem s WHERE " +
            "s.startDateTime < :endTime AND s.endDateTime > :startTime)")
     List<Venue> findAvailableVenues(@Param("startTime") LocalDateTime startTime, 
