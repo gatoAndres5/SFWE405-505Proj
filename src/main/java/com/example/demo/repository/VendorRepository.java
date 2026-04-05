3b19f3bd0e911978919b5b0a14c86bb714417478
sarahaljanabi <168122856+sarahaljanabi@users.noreply.github.com>
2026-04-05 14:07:03 -0700

    Add existsByContactEmail method to VendorRepository (#105)

--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Vendor;
 
-public interface VendorRepository extends JpaRepository<Vendor, Long>{
-    
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    boolean existsByContactEmail(String contactEmail);
 }
